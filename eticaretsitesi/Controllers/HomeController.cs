a29b2b849d6a59225d420ad93e95631cfd177104
muratapk <muratapk@hotmail.com>
2024-09-18 21:21:55 +0300

    ürün arama  v

--- eticaretsitesi/Controllers/HomeController.cs
+++ eticaretsitesi/Controllers/HomeController.cs
@@ -29,6 +29,38 @@ namespace eticaretsitesi.Controllers
             var result = _context.Products.Find(id);
             return View(result);
         }
+        public IActionResult AllCategories()
+        {
+            ViewBag.CategoryList=_context.Categories.ToList();
+            //kategori modelini groupjoin birleþtir product tablosu category tablosun category Id product tablosundaki categoryId eþitle
+            //category göre bunu groupla bunda productGroup ismi ver new yeni bir kolon türleri oluþtur burdan gelen verileri
+            //oluþturulan kolonlara ata
+
+            ViewBag.CategoryCount = _context.Categories.GroupJoin(_context.Products, category => category.CategoryId, product => product.CategoryId, (category, productGroup) => new { categoryName = category.CategoryName, productCount = productGroup.Count()}).ToList();
+
+            //groupjoin ifadesi 
+            ViewBag.LastProduct = _context.Products.OrderByDescending(p => p.CategoryId).Take(3).ToList();
+            var result=_context.Products.OrderByDescending(p=>p.ProductId).ToList();
+            return View(result);
+        }
+
+        [HttpGet]
+        public JsonResult ArtanAzalan(string Veri)
+        {
+            
+            if(Veri=="Artan")
+            {
+              var result = _context.Products.OrderByDescending(p => p.ProductPrice).ToList();
+                return Json(result);
+            }
+            else
+            {
+               var result = _context.Products.OrderByDescending(p => p.ProductPrice).ToList();
+                return Json(result);
+            }
+
+            
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
